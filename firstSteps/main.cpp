04fffb4d3681f6f85df1e7fc0247069553da9273
Sebastian Schalow <schalowseb@gmail.com>
2015-03-10 14:05:51 +0100

    - created main loop - first triangle drawn - added shaders

--- firstSteps/main.cpp
+++ firstSteps/main.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
 #include <GL/gl.h>
+#include <glm/glm.hpp>
 #include <string>
 
 
@@ -9,7 +10,8 @@
 bool init();
 void logSDLError(std::ostream& os, std::string);
 SDL_Window* setupWindow(SDL_Window *win, SDL_GLContext &ctx);
-void drawSomething(SDL_Window *win);
+void initGL(GLuint &vertexbuffer);
+void draw(GLuint &vertexbuffer);
 
 
 int main(int, char**){
@@ -17,12 +19,24 @@ int main(int, char**){
     SDL_Window *win = nullptr;
     SDL_GLContext ctx;
 
+    SDL_Event event;
+    GLuint vertexbuffer;
+
     init();
 
-    if(win = setupWindow(win, ctx)){
-        drawSomething(win);
-        SDL_Delay(1500);
-    }
+    win = setupWindow(win, ctx);
+
+    initGL(vertexbuffer);
+
+    // MAIN LOOP
+
+    do {
+
+        SDL_PollEvent(&event);
+        draw(vertexbuffer);
+        SDL_GL_SwapWindow(win);
+
+    } while(event.key.keysym.sym != SDLK_ESCAPE);
 
     return 0;
 	
@@ -36,6 +50,7 @@ bool init(){
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
@@ -64,11 +79,14 @@ SDL_Window* setupWindow(SDL_Window *win, SDL_GLContext &ctx){
         ctx = SDL_GL_CreateContext(win);
         SDL_GL_MakeCurrent(win, ctx);
 
+        // GLEW INITIALIZATION
         if(glewInit() < 0){
             logSDLError(std::cerr, "Initializing GLEW");
             SDL_DestroyWindow(win);
             win = nullptr;
             SDL_Quit();
+        } else {
+            glewExperimental = true;
         }
     }
 
@@ -77,20 +95,48 @@ SDL_Window* setupWindow(SDL_Window *win, SDL_GLContext &ctx){
 }
 
 
-void drawSomething(SDL_Window *win){
+void initGL(GLuint &vertexbuffer){
 
     GLuint VertexArrayID;
-
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
-    // Coordinates for the vertices describing a triangle
+    // An array of 3 vectors which represents 3 vertices
     static const GLfloat g_vertex_buffer_data[] = {
-           -1.0f, -1.0f, 0.0f,
-           1.0f, -1.0f, 0.0f,
-           0.0f,  1.0f, 0.0f,
-        };
+       -1.0f, -1.0f, 0.0f,
+       1.0f, -1.0f, 0.0f,
+       0.0f,  1.0f, 0.0f,
+    };
 
-    SDL_GL_SwapWindow(win);
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+}
+
+
+void draw(GLuint &vertexbuffer){
+
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+       0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+       3,                  // size
+       GL_FLOAT,           // type
+       GL_FALSE,           // normalized?
+       0,                  // stride
+       (void*)0            // array buffer offset
+    );
+
+    // Draw the triangle !
+    glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+    glDisableVertexAttribArray(0);
 
 }
