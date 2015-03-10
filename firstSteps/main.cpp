575b281cd471b6c7749633d2891eccba007fb784
Sebastian Schalow <schalowseb@gmail.com>
2015-03-10 18:27:22 +0100

    some comments & co.

--- firstSteps/main.cpp
+++ firstSteps/main.cpp
@@ -12,7 +12,7 @@ void logSDLError(std::ostream& os, std::string);
 SDL_Window* setupWindow(SDL_Window *win, SDL_GLContext &ctx);
 void initGL(GLuint &vertexbuffer);
 void draw(GLuint &vertexbuffer);
-
+void clearAll(SDL_Window *win, SDL_GLContext &ctx);
 
 int main(int, char**){
 	
@@ -20,23 +20,25 @@ int main(int, char**){
     SDL_GLContext ctx;
 
     SDL_Event event;
-    GLuint vertexbuffer;
+    GLuint vbo_Vertexbuffer;
 
     init();
 
     win = setupWindow(win, ctx);
 
-    initGL(vertexbuffer);
+    initGL(vbo_Vertexbuffer);
 
     // MAIN LOOP
 
     do {
 
         SDL_PollEvent(&event);
-        draw(vertexbuffer);
+        draw(vbo_Vertexbuffer);
         SDL_GL_SwapWindow(win);
 
-    } while(event.key.keysym.sym != SDLK_ESCAPE);
+    } while(event.key.keysym.sym != SDLK_ESCAPE && event.type != SDL_QUIT);
+
+    clearAll(win, ctx);
 
     return 0;
 	
@@ -97,24 +99,25 @@ SDL_Window* setupWindow(SDL_Window *win, SDL_GLContext &ctx){
 
 void initGL(GLuint &vertexbuffer){
 
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
+    GLuint VAO_VertexArrayID;
+    // Allocate and assign 1 Vertex Array Object to our handle
+    glGenVertexArrays(1, &VAO_VertexArrayID);
+    glBindVertexArray(VAO_VertexArrayID);
 
     // An array of 3 vectors which represents 3 vertices
     static const GLfloat g_vertex_buffer_data[] = {
-       -1.0f, -1.0f, 0.0f,
-       1.0f, -1.0f, 0.0f,
-       0.0f,  1.0f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+       0.0f, 0.5f, 0.0f,
+       0.5f, -0.5f, 0.0f,
     };
 
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    // Generate 1 Vertex Buffer Object, put the resulting identifier in vertexbuffer
     glGenBuffers(1, &vertexbuffer);
 
-    // The following commands will talk about our 'vertexbuffer' buffer
+    // Bind our VBO as being the active buffer and storing vertex attributes (coordinates)
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 
-    // Give our vertices to OpenGL.
+    // Give our vertices to OpenGL
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 }
@@ -122,21 +125,31 @@ void initGL(GLuint &vertexbuffer){
 
 void draw(GLuint &vertexbuffer){
 
-    // 1rst attribute buffer : vertices
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Specify that our coordinate data is going into attribute index 0, and contains 3 floats per vertex
     glVertexAttribPointer(
        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
        3,                  // size
        GL_FLOAT,           // type
        GL_FALSE,           // normalized?
        0,                  // stride
-       (void*)0            // array buffer offset
+       0                   // array buffer offset
     );
+
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
 
     // Draw the triangle !
     glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 
     glDisableVertexAttribArray(0);
+
+}
+
+
+void clearAll(SDL_Window *win, SDL_GLContext &ctx){
+
+    SDL_GL_DeleteContext(ctx);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
 }
